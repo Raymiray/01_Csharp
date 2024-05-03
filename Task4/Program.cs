bf29a14cbeb8a08079329b7bc0043898ec5f9fb9
Raymiray <ritsiscream11@yandex.ru>
2024-05-03 18:18:05 +0300

    add lesson1:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 15;
+int b = 9;
+int c = 13;
+int d = 4;
+int e = 11;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
