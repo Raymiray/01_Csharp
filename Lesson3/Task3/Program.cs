4d61fe2af412ba42d9d8e6e39655797f09639349
Raymiray <ritsiscream11@yandex.ru>
2024-05-17 20:30:39 +0300

    add lesson3: task3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// void Foo(int a)
+// {
+//     Console.WriteLine(a);
+// }
+
+// int value = 5;
+// Foo(value);
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEvenElements(array);
+PrintArray(array);
