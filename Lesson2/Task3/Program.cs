5bb772649d43017c464f6e20aa21b54d212faf23
Raymiray <ritsiscream11@yandex.ru>
2024-05-17 19:51:21 +0300

    объединение лекций в одну папку

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 2, 5, 3, 6, 39, 10, 4, 6, 59, 41 };
+int i = 0;
+
+while (i < n) //while (i < array.Lenght)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
