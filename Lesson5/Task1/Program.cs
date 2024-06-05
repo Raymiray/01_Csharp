7086b2e284ff893674d8aeb26b1ceb6a76110d50
Raymiray <ritsiscream11@yandex.ru>
2024-06-05 16:02:32 +0300

    add lesson 5 task1

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int Fact(int n) // так делается только для примера. Факториал ищется через ципл for
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+
+Console.Write(Fact(5));
+
+//F11 шаг с заходом
+//F10 шаг с обходом
+//F5 продолжить (до следующей точки)
